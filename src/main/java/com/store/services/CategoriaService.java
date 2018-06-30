d3e9eecec022a960f1083518701e7e47f3781148
stephano <stephanobalbinot@hotmail.com>
2018-06-30 20:57:36 -0300

    Métodos CRUD na Categoria, criação de exceptions e pequenos ajustes

--- src/main/java/com/store/services/CategoriaService.java
+++ src/main/java/com/store/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.store.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.store.domain.Categoria;
 import com.store.repositories.CategoriaRepository;
+import com.store.services.exceptions.DataIntegrityException;
 import com.store.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,9 +17,28 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 
 }
