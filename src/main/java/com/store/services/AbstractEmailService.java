b5928b831e218c5ba4782167460ad38355630985
stephano <stephanobalbinot@hotmail.com>
2018-07-02 10:09:23 -0300

    Email HTML com spring-thymeleaf

--- src/main/java/com/store/services/AbstractEmailService.java
+++ src/main/java/com/store/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.store.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.store.domain.Pedido;
 
@@ -12,10 +20,26 @@ public abstract class AbstractEmailService implements EmailService {
 	@Value("${default.sender}")
 	private String sender;
 	
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage smm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(smm);
+	}
+	
+	@Override
+	public void sendOrderConfirmationEmailHtml(Pedido obj) {
+		try {
+			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+			sendEmailHtml(mm);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
 	}
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
@@ -28,4 +52,20 @@ public abstract class AbstractEmailService implements EmailService {
 		return smm;
 	}
 	
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mm = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mm, true);
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj), true);
+		return mm;
+	}
+	
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		return templateEngine.process("email/confirmacaoPedido", context);
+	}
 }
--- src/main/java/com/store/services/EmailService.java
+++ src/main/java/com/store/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.store.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.store.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendEmail(SimpleMailMessage msg);
 	
 	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmailHtml (MimeMessage msg);
+	
+	void sendOrderConfirmationEmailHtml(Pedido obj);
 	
 }
