42284f036ce5ca5bb44087cc6adb5deaf0c5f231
stephano <stephanobalbinot@hotmail.com>
2018-07-04 21:00:31 -0300

    Ajustando tamanho da imagem de perfil do Cliente

--- src/main/java/com/store/services/ClienteService.java
+++ src/main/java/com/store/services/ClienteService.java
@@ -50,6 +50,9 @@ public class ClienteService {
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -130,8 +133,8 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
-		/*jpgImage = imageService.cropSquare(jpgImage);
-		jpgImage = imageService.resize(jpgImage, size);*/
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		
 		String fileName = prefix + user.getId() + ".jpg";
 		
